6735cab0d990638ea9fe98a104d0be3627582862
mmassime <56897994+mmassime@users.noreply.github.com>
2022-04-08 00:16:10 +0200

    generate_model

--- generate_model.py
+++ generate_model.py
@@ -1,4 +1,7 @@
+from msilib.schema import Binary
 import sys
+
+import constraint
 
 
 
@@ -7,10 +10,55 @@ def parseFile(path):
     parameters = f.readline().rstrip().split(" ")
     obj, types, conflicts = parameters[0], parameters[1], parameters[2]
     c = f.readline().rstrip()
-    resList=[]
+    parmatersTable = []
     for i in range(int(types)):
-        resList.append([int(element)for element in f.readline().rstrip().split(" ")])
-    print(resList)
+        parmatersTable.append([int(element)for element in f.readline().rstrip().split(" ")])
+    
+    conflictsTable = []
+    for i in range(int(conflicts)):
+        conflictsTable.append([int(element)for element in f.readline().rstrip().split(" ")])
+    
+    res = open(path + ".lp", "w")
+    res.write("Minimize \n")
+    obj_function = "x_0"
+    for i in range(1,int(obj)):
+        obj_function += (" + x_"+str(i))
+    res.write("     " + obj_function + "\n")
+    res.write("Subject To \n")
+    constraintList = []
+    ctr_1 = ""
+    for i in range(int(obj)):
+        for j in range(int(obj)):
+            ctr_1 += "y_{0}_{1} + ".format(int(i), int(j))
+    ctr_1 = ctr_1[:len(ctr_1)-3] + " = " + str(obj)
+    constraintList.append(ctr_1)
+    s = []
+    for param in parmatersTable:
+        for i in range(param[1]):
+            s.append(param[0])
+    
+    for i in range(int(obj)):
+        ctr_2 = ""
+        for j in range(int(obj)):
+            ctr_2 += "y_{0}_{1} * {2} + ".format(str(i), str(j), s[j])
+        ctr_2 = ctr_2[:len(ctr_2)-3] + " <= " + c
+        constraintList.append(ctr_2)
+        
+    index = 0
+    for ctr in constraintList:
+        res.write("    ctr_{0}: ".format(index) + ctr + "\n")
+        index += 1
+    
+    res.write("Binary \n")
+    for i in range(int(obj)):
+        res.write("     x_{0}".format(i) + "\n")
+    for i in range(int(obj)):
+        for j in range(int(obj)):
+            res.write("     y_{0}_{1}".format(i, j) + "\n")
+    res.write("End")
+    
+    
+        
 
 
 
