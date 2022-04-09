773426262043608aa8f667893c074cc2534373bb
mmassime <56897994+mmassime@users.noreply.github.com>
2022-04-10 00:25:54 +0200

    nouvelle version

--- generate_model.py
+++ generate_model.py
@@ -8,7 +8,7 @@ def parseFile(path):
     f = open(path, "r")
     parameters = f.readline().rstrip().split(" ")
     obj, types, conflicts = parameters[0], parameters[1], parameters[2]
-    c = f.readline().rstrip()
+    q = f.readline().rstrip()
     parmatersTable = []
     for i in range(int(types)):
         parmatersTable.append([int(element)for element in f.readline().rstrip().split(" ")])
@@ -19,35 +19,26 @@ def parseFile(path):
     
     res = open(path + ".lp", "w")
     res.write("Minimize \n")
-    obj_function = "x_0"
-    for i in range(1,int(obj)):
-        obj_function += (" + x_"+str(i))
+    obj_function = "z_0"
+    for c in range(1,int(obj)):
+        obj_function += (" + z_"+str(c))
     res.write("     " + obj_function + "\n")
     res.write("Subject To \n")
     constraintList = []
-    ctr_1 = ""
-    for i in range(int(obj)):
-        for j in range(int(obj)):
-            ctr_1 += "y_{0}_{1} + ".format(int(i), int(j))
-    ctr_1 = ctr_1[:len(ctr_1)-3] + " = " + str(obj)
-    #constraintList.append(ctr_1)
-    s = []
-    for param in parmatersTable:
-        for i in range(param[1]):
-            s.append(param[0])
-    
-    for i in range(int(obj)):
-        ctr_2 = ""
-        for j in range(int(obj)):
-            ctr_2 += "{2}y_{0}_{1} + ".format(str(i), str(j), s[j])
-        ctr_2 = ctr_2[:len(ctr_2)-3] + " - " + c + " x_{0}".format(i) + " <= 0" 
-        constraintList.append(ctr_2)
 
-    for j in range(int(obj)):
+    
+    for c in range(int(obj)):
+        ctr_1 = ""
+        for t in range(int(types)):
+            ctr_1 += "{2}x_{0}_{1} + ".format(str(c), str(t), parmatersTable[t][0])
+        ctr_1 = ctr_1[:len(ctr_1)-3] + " - " + q + " z_{0}".format(c) + " <= 0" 
+        constraintList.append(ctr_1)
+
+    for t in range(int(types)):
         ctr_2 = ""
-        for i in range(int(obj)):
-            ctr_2 += "y_{0}_{1} + ".format(str(i), str(j))
-        ctr_2 = ctr_2[:len(ctr_2)-3] +  " = 1" 
+        for c in range(int(obj)):
+            ctr_2 += "x_{0}_{1} + ".format(str(c), str(t))
+        ctr_2 = ctr_2[:len(ctr_2)-3] +  " = " + str(parmatersTable[t][1]) 
         constraintList.append(ctr_2)
         
     index = 0
@@ -56,11 +47,15 @@ def parseFile(path):
         index += 1
     
     res.write("Binary \n")
-    for i in range(int(obj)):
-        res.write("     x_{0}".format(i) + "\n")
-    for i in range(int(obj)):
-        for j in range(int(obj)):
-            res.write("     y_{0}_{1}".format(i, j) + "\n")
+    for c in range(int(obj)):
+        res.write("     z_{0}".format(c) + "\n")
+    for c in range(int(obj)):
+        for t in range(int(types)):
+            res.write("     y_{0}_{1}".format(c, t) + "\n")
+    res.write("Integer \n")
+    for c in range(int(obj)):
+        for t in range(int(types)):
+            res.write("     x_{0}_{1}".format(c, t) + "\n")   
     res.write("End")
     
     
