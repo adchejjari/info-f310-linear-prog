294c128b3cf225008e33c0a672a5bfaf83194a4f
mmassime <56897994+mmassime@users.noreply.github.com>
2022-04-16 00:09:41 +0200

    clean code

--- generate_model.py
+++ generate_model.py
@@ -1,107 +1,114 @@
-#from msilib.schema import Binary
 import sys
 
-
-
-
-def parseFile(path):
-    f = open(path, "r")
-    parameters = f.readline().rstrip().split(" ")
-    obj, types, conflicts = parameters[0], parameters[1], parameters[2]
-    q = f.readline().rstrip()
-    parmatersTable = []
-    for i in range(int(types)):
-        parmatersTable.append([int(element)for element in f.readline().rstrip().split(" ")])
-    M  = max(tuple(zip(*parmatersTable))[1])
-    conflictsTable = []
-    for i in range(int(conflicts)):
-        conflictsTable.append([int(element)for element in f.readline().rstrip().split(" ")])
+class ModelGenerator:
     
-    res = open(path + ".lp", "w")
-    
-    #obj function
-    res.write("Minimize \n")
-    obj_function = "z_0"
-    for c in range(1,int(obj)):
-        obj_function += (" + z_"+str(c))
-    res.write("     " + obj_function + "\n")
-    
-    #contraintes
-    res.write("Subject To \n")
-    constraintList = []
-    
-    for c in range(int(obj)):
-        ctr_1 = ""
-        for t in range(int(types)):
-            ctr_1 += "{2}x_{0}_{1} + ".format(str(c), str(t), parmatersTable[t][0])
-        ctr_1 = ctr_1[:len(ctr_1)-3] + " - " + q + " z_{0}".format(c) + " <= 0" 
-        constraintList.append(ctr_1)
-
-    for t in range(int(types)):
-        ctr_2 = ""
-        for c in range(int(obj)):
-            ctr_2 += "x_{0}_{1} + ".format(str(c), str(t))
-        ctr_2 = ctr_2[:len(ctr_2)-3] +  " = " + str(parmatersTable[t][1]) 
-        constraintList.append(ctr_2)
+    def __init__(self, filePath, p):
         
-    for t in range(int(types)):
-        for c in range(int(obj)):
-            ctr_3 = "y_{0}_{1} - x_{0}_{1} <= 0".format(str(c), str(t))
-            constraintList.append(ctr_3)
+        self.filePath = filePath
+        self.p = int(p)
+        self.parseParameters()
         
-    for t in range(int(types)):
-        for c in range(int(obj)):
-            ctr_4 = "x_{0}_{1} - {2}y_{0}_{1} <= 0".format(str(c), str(t), str(M))
-            constraintList.append(ctr_4)
-          
-    if(int(args[2]) > 0):
-        #special contrainte 1
-        for t in range(int(types)):
-            ctr_5 = ""
-            for c in range(int(obj)):
-                ctr_5 += "y_{0}_{1} + ".format(str(c), str(t))
-            ctr_5 = ctr_5[:len(ctr_5)-3] +  " <= " + str(parmatersTable[t][2]) 
-            constraintList.append(ctr_5)
-
-    if (int(args[2]) == 2):
-        #special constraint n 2
-        for c in range(int(obj)):
-            for conflict in conflictsTable:
-                ctr_6 = ""
-                for type in conflict:
-                    ctr_6 += "y_{0}_{1} + " .format(str(type), c)
-                ctr_6 = ctr_6[:len(ctr_6)-3] + " <= 1"
-                constraintList.append(ctr_6)
-
-    
-                  
-    index = 0
-    for ctr in constraintList:
-        res.write("    ctr_{0}: ".format(index) + ctr + "\n")
-        index += 1
-    
-    #variables
-    res.write("Binary \n")
-    for c in range(int(obj)):
-        res.write("     z_{0}".format(c) + "\n")
-    for c in range(int(obj)):
-        for t in range(int(types)):
-            res.write("     y_{0}_{1}".format(c, t) + "\n")
-    res.write("Integer \n")
-    for c in range(int(obj)):
-        for t in range(int(types)):
-            res.write("     x_{0}_{1}".format(c, t) + "\n")   
-    res.write("End")
-    
-    
+    def parseParameters(self):
         
+        f = open(self.filePath, "r")
+        parameters = f.readline().rstrip().split(" ")
+        # n = nombre d'objets, T = nombre de types, I = nombre d'incompatibilites
+        self.n, self.T, I = int(parameters[0]), int(parameters[1]), int(parameters[2])
+        self.q = f.readline().rstrip()  #capacité de la boite
+        self.parmatersTable = []
+        for t in range(self.T):
+            # [taille du type t, nbr d'objets du type t, b_t]
+            self.parmatersTable.append([int(element)for element in f.readline().rstrip().split(" ")])
+        self.M  = max(tuple(zip(*self.parmatersTable))[1])  #big M 
+        self.conflictsTable = []
+        for i in range(I):
+            self.conflictsTable.append([int(element)for element in f.readline().rstrip().split(" ")])
+        
+    def generateModel(self):
+        
+        model = open(self.filePath + ".lp", "w")
+    
+        #fonction objet
+        model.write("Minimize \n")
+        obj_function = "z_0"
+        
+        for c in range(1,self.n):
+            obj_function += (" + z_"+str(c))
+        model.write("     " + obj_function + "\n")
+        
+        #contraintes
+        model.write("Subject To \n")
+        constraintList = []
+        
+        for c in range(self.n):
+            ctr_1 = ""
+            for t in range(self.T):
+                ctr_1 += "{2}x_{0}_{1} + ".format(str(c), str(t), self.parmatersTable[t][0])
+            ctr_1 = ctr_1[:len(ctr_1)-3] + " - " + self.q + " z_{0}".format(c) + " <= 0" 
+            constraintList.append(ctr_1)
 
+        for t in range(self.T):
+            ctr_2 = ""
+            for c in range(self.n):
+                ctr_2 += "x_{0}_{1} + ".format(str(c), str(t))
+            ctr_2 = ctr_2[:len(ctr_2)-3] +  " = " + str(self.parmatersTable[t][1]) 
+            constraintList.append(ctr_2)
+            
+        for t in range(self.T):
+            for c in range(self.n):
+                ctr_3 = "y_{0}_{1} - x_{0}_{1} <= 0".format(str(c), str(t))
+                constraintList.append(ctr_3)
+            
+        for t in range(self.T):
+            for c in range(self.n):
+                ctr_4 = "x_{0}_{1} - {2}y_{0}_{1} <= 0".format(str(c), str(t), str(self.M))
+                constraintList.append(ctr_4)
+        
+        if(self.p == 1 or self.p == 2):
+            #premiere contrainte additionelle 
+            for t in range(self.T):
+                ctr_5 = ""
+                for c in range(self.n):
+                    ctr_5 += "y_{0}_{1} + ".format(str(c), str(t))
+                ctr_5 = ctr_5[:len(ctr_5)-3] +  " <= " + str(self.parmatersTable[t][2]) 
+                constraintList.append(ctr_5)
 
+        if (self.p == 2):
+            #special constraint n 2
+            for c in range(self.n):
+                for conflict in self.conflictsTable:
+                    ctr_6 = ""
+                    for t in conflict:
+                        ctr_6 += "y_{0}_{1} + " .format(str(t), c)
+                    ctr_6 = ctr_6[:len(ctr_6)-3] + " <= 1"
+                    constraintList.append(ctr_6)
+
+        index = 0
+        for ctr in constraintList:
+            model.write("    ctr_{0}: ".format(index) + ctr + "\n")
+            index += 1
+        
+        #variables
+        model.write("Binary \n")
+        for c in range(self.n):
+            model.write("     z_{0}".format(c) + "\n")
+        for c in range(self.n):
+            for t in range(self.T):
+                model.write("     y_{0}_{1}".format(c, t) + "\n")
+                
+        model.write("Integer \n")
+        for c in range(self.n):
+            for t in range(self.T):
+                model.write("     x_{0}_{1}".format(c, t) + "\n")
+                   
+        model.write("End")        
+
+    
 if __name__ == '__main__':
+    
     args = sys.argv
-    instance = args[1]
-    p = int(args[2])
-    parseFile(instance)
+    modelGenerator = ModelGenerator(args[1], args[2])
+    modelGenerator.generateModel()
 
 
 
